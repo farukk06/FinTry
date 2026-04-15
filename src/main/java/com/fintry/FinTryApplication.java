76388bdfdb4d84d5b36ed37fb53d2cbca8338664
Faruk Turnalı <farukturnali11@gmail.com>
2026-04-15 14:36:27 +0300

    InÄitial project setup

---
+++ src/main/java/com/fintry/FinTryApplication.java
@@ -0,0 +1,13 @@
+package com.fintry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinTryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fintry/FinTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
